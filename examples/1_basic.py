2b709681c722f511cfabd6d06399f6af05dcec9e
rensdimmendaal <rens@dimmendaal.com>
2021-10-16 14:59:48 +0200

    prettier printing, #%% for stepwise evaluation, show export options

--- examples/1_basic.py
+++ examples/1_basic.py
@@ -3,26 +3,34 @@ from datetime import datetime
 from typing import List, Optional
 from pydantic import BaseModel
 from dataclasses import dataclass
+from rich import print
+from rich.traceback import install
+
+install(show_locals=True)
+
 # Pydantic is useful to get data from external sources, such as API's.
 # It can do data validation and can enforce type hints.
 
+
 class User(BaseModel):
     id: int
-    name = 'John Doe'
+    name = "John Doe"
     signup_ts: Optional[datetime] = None
     friends: Optional[List[int]] = None
+
 
 @dataclass
 class DataClassUser:
     id: int
-    name = 'John Doe'
+    name = "John Doe"
     signup_ts: Optional[datetime] = None
-    friends: Optional[List[int]] = None 
+    friends: Optional[List[int]] = None
+
 
 external_data = {
-    'id': '123',
-    'signup_ts': '2019-06-01 12:22',
-    'friends': [1, 2, '3'],
+    "id": "123",
+    "signup_ts": "2019-06-01 12:22",
+    "friends": [1, 2, "3"],
 }
 
 user = User(**external_data)
@@ -35,9 +43,9 @@ print(data_class_user)
 
 # It enforces type hints (but also does automatic conversions...)
 external_data_broken = {
-    'id': [123, 456],
-    'signup_ts': '2019-06-01 12:22',
-    'friends': [1, 2, '3'],
+    "id": [123, 456],
+    "signup_ts": "2019-06-01 12:22",
+    "friends": [1, 2, "3"],
 }
 data_class_user = DataClassUser(**external_data_broken)
 print(data_class_user)
--- examples/3_aliases.py
+++ examples/3_aliases.py
@@ -1,47 +1,65 @@
+#%%
 from pydantic import BaseModel
 from pydantic.fields import Field
 
-# You're communicating with some API that uses camelCase, but you want to use snake_case.
-class User(BaseModel):
-    first_name: str = Field(alias="firstName")
-    last_name: str = Field(alias="lastName")
+from rich import print
+from rich.traceback import install
 
-external_data = {"firstName": "Jan", "lastName": "Klaasse"}
+# install(show_locals=False)  # pretty print errors
 
-user = User(**external_data)
-
-print(user.first_name)
-print(user.last_name)
-
-# By default though, the following does not work.
-try:
-    user = User(first_name="Jan", last_name="Klaasse")
-except Exception as e:
-    print(e)
-
-# Why? You cannot assign by field_name by default. We can fix this though.
-class User(BaseModel):
-    first_name: str = Field(alias="firstName")
-    last_name: str = Field(alias="lastName")
-    class Config:
-        allow_population_by_field_name = True
-
-user = User(first_name="Jan", last_name="Klaasse")
-print(user.json())
-
-# Adding all aliases becomes tedious for large inputs....
-def to_camel(snake_str: str) -> str:
-    components = snake_str.split('_')
-    return components[0] + ''.join(x.title() for x in components[1:])
-
-
+#%% You're communicating with some API that uses camelCase, but you want to use snake_case.
 class User(BaseModel):
     first_name: str
     last_name: str
-    class Config:
-        allow_population_by_field_name = True
-        alias_generator = to_camel
+
 
 external_data = {"firstName": "Jan", "lastName": "Klaasse"}
+
+# This fails
 user = User(**external_data)
-print(user)
+
+#%% We can fix that with field aliases
+class User(BaseModel):
+    first_name: str = Field(alias="firstName")  # <-- The Fix!
+    last_name: str = Field(alias="lastName")  # <-- The Fix!
+
+
+user = User(**external_data)
+print(f"{user=}")
+
+#%% Now we have a new problem. Now this won't work:
+user = User(first_name="Jan", last_name="Klaasse")
+
+# %%Why? You cannot assign by field_name by default. We can fix this though.
+class User(BaseModel):
+    first_name: str = Field(alias="firstName")
+    last_name: str = Field(alias="lastName")
+
+    class Config:
+        allow_population_by_field_name = True  # <- The Fix!
+
+
+user = User(first_name="Jan", last_name="Klaasse")
+print(f"{user=}")
+
+#%% Adding all aliases becomes tedious for large inputs....
+def to_camel_case(snake_str: str) -> str:
+    components = snake_str.split("_")
+    return components[0] + "".join(x.title() for x in components[1:])
+
+
+class User(BaseModel):
+    first_name: str  # <-- Look no more Field aliases!
+    last_name: str
+
+    class Config:
+        allow_population_by_field_name = True
+        alias_generator = to_camel_case  # <-- The Fix!
+
+
+user = User(**external_data)
+print(f"{user=}")
+
+#%% you can also export by alias or by the attribute name
+print(f"{user.dict(by_alias=False)=}")
+print(f"{user.dict(by_alias=True)=}")

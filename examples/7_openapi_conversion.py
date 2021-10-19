3c9c5e3dc7a99b96dd66b93272593f2b856f9bec
Daniel Willemsen <danielwillemsen@live.nl>
2021-10-19 08:25:32 +0200

    added conversion

--- examples/7_openapi_conversion.py
+++ examples/7_openapi_conversion.py
@@ -1,1 +1,12 @@
 # TODO(Daniel)
+# %%
+# Generate a model from an example json.
+# datamodel-codegen --input-file-type json --input example.json --output menu.py
+
+# %% 
+import json
+from menu import Model
+
+menu = Model.parse_file("example.json")
+
+print(menu)
